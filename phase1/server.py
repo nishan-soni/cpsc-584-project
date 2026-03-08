2dbd5d17fe3dce5ab9b891af1a985fb1e1570059
Matias Campuzano <matiascampuzano2013@gmail.com>
2026-03-07 23:55:02 -0700

    higher

--- phase1/server.py
+++ phase1/server.py
@@ -83,28 +83,19 @@ def start_server():
                         elif command == 'cam_stop':
                             crawler.do_action('stand', 1, current_speed)
                             
-                        # --- THE TACTICAL BUMPERS ---
-                        elif command == 'tripwire':
-                            # Flare all 4 legs fully outward (Y=90) and press into the ground (-90) for maximum grip
-                            crawler.do_step([[50,90,-90], [50,90,-90], [50,90,-90], [50,90,-90]], current_speed)
-                        elif command == 'silent_mode':
-                            # Drops flat to ground and kills servo power to remove motor hum
-                            crawler.do_step([[0,0,0], [0,0,0], [0,0,0], [0,0,0]], current_speed)
-                            
                         # --- THE SPY POSTURES (Face Buttons) ---
                         elif command == 'high_posture':
-                            z_height = -110
-                            crawler.do_step([[50,50,z_height], [50,50,z_height], [50,50,z_height], [50,50,z_height]], current_speed)
+                            crawler.do_step([[50,50,-90], [50,50,-90], [50,50,-90], [50,50,-90]], current_speed)
                         elif command == 'stealth_mode':
-                            z_height = -20
-                            crawler.do_step([[50,50,z_height], [50,50,z_height], [50,50,z_height], [50,50,z_height]], current_speed)
+                            crawler.do_step([[50,50,-30], [50,50,-30], [50,50,-30], [50,50,-30]], current_speed)
                         elif command == 'strafe_left':
-                            crawler.do_step([[50,90,z_height], [50,10,z_height], [50,10,z_height], [50,90,z_height]], current_speed)
+                            # Right legs push OUT (Y=90), Left legs pull IN (Y=10) -> Body shifts strictly left
+                            crawler.do_step([[50,90,-60], [50,10,-60], [50,10,-60], [50,90,-60]], current_speed)
                         elif command == 'strafe_right':
-                            crawler.do_step([[50,10,z_height], [50,90,z_height], [50,90,z_height], [50,10,z_height]], current_speed)
+                            # Right legs pull IN (Y=10), Left legs push OUT (Y=90) -> Body shifts strictly right
+                            crawler.do_step([[50,10,-60], [50,90,-60], [50,90,-60], [50,10,-60]], current_speed)
                         elif command == 'stand':
-                            z_height = -60
-                            crawler.do_step([[50,50,z_height], [50,50,z_height], [50,50,z_height], [50,50,z_height]], current_speed)
+                            crawler.do_action('stand', 1, current_speed)
                             
                 except socket.timeout:
                     # Timeout reached, meaning we haven't received a new command, 
@@ -116,14 +107,11 @@ def start_server():
                 
                 # Execute continuous movement
                 if current_move == 'forward':
-                    # Custom walking gait that uses our current z_height instead of the default do_action
-                    crawler.do_step([[50,50,z_height], [50,0,z_height], [50,50,z_height], [50,0,z_height]], current_speed)
-                    crawler.do_step([[50,100,z_height], [50,50,z_height], [50,100,z_height], [50,50,z_height]], current_speed)
+                    crawler.do_action('forward', 1, current_speed)
                 elif current_move == 'back':
-                    crawler.do_step([[50,100,z_height], [50,50,z_height], [50,100,z_height], [50,50,z_height]], current_speed)
-                    crawler.do_step([[50,50,z_height], [50,0,z_height], [50,50,z_height], [50,0,z_height]], current_speed)
+                    crawler.do_action('backward', 1, current_speed)
                 elif current_move == 'right':
-                    crawler.do_action('turn right', 1, current_speed) # Turning is complex, leave default for now
+                    crawler.do_action('turn right', 1, current_speed)
                 elif current_move == 'left':
                     crawler.do_action('turn left', 1, current_speed)
 
