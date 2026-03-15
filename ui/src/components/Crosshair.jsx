42f724d947ff6ef5655de66525e57635c811b7e1
nishan-soni <nishan.soni2@gmail.com>
2026-03-15 01:11:02 -0600

    initial

---
+++ ui/src/components/Crosshair.jsx
@@ -0,0 +1,16 @@
+
+/**
+ * Crosshair overlay. To ensure its in the center we make it an overlay that takes the full parent's size
+ */
+function Crosshair() {
+
+    return (
+        <div className="flex flex-col w-full h-full text-6xl justify-center items-center">
+            [ + ]
+        </div>
+    )
+
+}
+
+
+export default Crosshair
---
+++ ui/src/components/Hud.jsx
@@ -0,0 +1,15 @@
+
+import Crosshair from './Crosshair'
+
+function Hud() {
+
+    return (
+        <div className="flex flex-col h-10/12 w-10/12 m-auto border-2">
+            <Crosshair/>
+        </div>
+    )
+
+}
+ 
+
+export default Hud
